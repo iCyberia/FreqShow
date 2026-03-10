2640f856c902a05057f861252c1cf1645d7b75f7
Hiroshi Thomas <hdt713@gmail.com>
2026-03-10 11:50:53 -0400

    update waterfall speed intervals for improved responsiveness

--- model.py
+++ model.py
@@ -53,7 +53,7 @@ class FreqShowModel(object):
                 self.set_gain(10)
                 self.waterfall_speed_index = 1
                 self.waterfall_speed_labels = ['SLOW', 'MED', 'FAST', 'MAX']
-                self.waterfall_speed_intervals = [1.0, 1.0 / 3.0, 0.1, 0.05]
+                self.waterfall_speed_intervals = [1.0, 1.0 / 3.0, 0.1, 0.01]
 
         def get_waterfall_speed_label(self):
                 return self.waterfall_speed_labels[self.waterfall_speed_index]
